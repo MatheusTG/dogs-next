57b6f7b356e7609a713ac1230e71c78fd5dc55e5
Matheus <matheusteodorogarcia@gmail.com>
2024-07-13 15:03:36 -0300

    Infinite scroll

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -16,7 +16,7 @@ export default async function ContaPage() {
   return (
     <div>
       {data?.length ? (
-        <Feed photos={data} />
+        <Feed photos={data} user={user?.username} />
       ) : (
         <div>
           <p
--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,16 +1,69 @@
 "use client";
 
+import React from "react";
 import FeedPhotos from "./FeedPhotos";
-import { Photo } from "@/actions/photosGet";
+import photosGet, { Photo } from "@/actions/photosGet";
 
 type Props = {
   photos: Photo[];
+  user?: 0 | string;
 };
 
-export default function Feed({ photos }: Props) {
+export default function Feed({ photos, user }: Props) {
+  const [photosFeed, setPhotosFeed] = React.useState<Photo[]>(photos);
+  const [page, setPage] = React.useState(1);
+  const [loading, setLoading] = React.useState(false);
+  const [infinite, setInfinite] = React.useState(photos.length >= 6);
+
+  const fetching = React.useRef(false);
+
+  function infiniteScroll() {
+    if (fetching.current) return;
+    fetching.current = true;
+    setTimeout(() => {
+      setPage((currentPage) => currentPage + 1);
+      fetching.current = false;
+      setLoading(false);
+    }, 1000);
+  }
+
+  React.useEffect(() => {
+    if (page === 1) return;
+    async function getPagePhotos(page: number) {
+      const actionData = await photosGet(
+        { page, total: 6, user: 0 },
+        {
+          cache: "no-cache",
+        }
+      );
+      if (actionData && actionData.data !== null) {
+        const { data } = actionData;
+        setPhotosFeed((currentPhotos) => [...currentPhotos, ...data]);
+        if (data.length < 6) setInfinite(false);
+      }
+    }
+    getPagePhotos(page);
+  }, [page]);
+
+  React.useEffect(() => {
+    if (infinite) {
+      setLoading(true);
+      window.addEventListener("scroll", infiniteScroll);
+      window.addEventListener("wheel", infiniteScroll);
+    } else {
+      window.removeEventListener("scroll", infiniteScroll);
+      window.removeEventListener("wheel", infiniteScroll);
+    }
+    return () => {
+      window.removeEventListener("scroll", infiniteScroll);
+      window.removeEventListener("wheel", infiniteScroll);
+    };
+  }, [infinite]);
+
   return (
     <div>
-      <FeedPhotos photos={photos} />
+      <FeedPhotos photos={photosFeed} />
+      {loading && <p>Carregando...</p>}
     </div>
   );
 }
