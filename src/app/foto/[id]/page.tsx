7cfe3243f9ac52503ebd83ee2e090aec52537efe
Matheus <matheusteodorogarcia@gmail.com>
2024-07-14 11:34:58 -0300

    Photo page

--- src/app/foto/[id]/page.tsx
+++ src/app/foto/[id]/page.tsx
@@ -1,11 +1,29 @@
-export default async function FotoIdPage({
-  params,
-}: {
-  params: { id: number };
-}) {
+import photoGet from "@/actions/photoGet";
+import PhotoContent from "@/components/photo/PhotoContent";
+import { notFound } from "next/navigation";
+
+type FotoIdParams = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({ params }: FotoIdParams) {
+  const { data } = await photoGet(params.id);
+
+  if (!data) return { title: "Fotos" };
+  return {
+    title: data.photo.title,
+  };
+}
+
+export default async function FotoIdPage({ params }: FotoIdParams) {
+  const { data } = await photoGet(params.id);
+
+  if (!data) notFound();
   return (
     <section className="container mainContainer">
-      <h1>{params.id}</h1>
+      <PhotoContent data={data} single={true} />
     </section>
   );
 }
