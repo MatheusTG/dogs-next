f6b7eb7493bce809193a38998530dba143183879
Matheus <matheusteodorogarcia@gmail.com>
2024-07-14 21:25:28 -0300

    Profile page

--- src/app/perfil/[user]/page.tsx
+++ src/app/perfil/[user]/page.tsx
@@ -1,11 +1,18 @@
-export default async function PerfilUserPage({
-  params,
-}: {
+import photosGet from "@/actions/photosGet";
+import Feed from "@/components/Feed/Feed";
+
+type Props = {
   params: { user: string };
-}) {
+};
+
+export default async function PerfilUserPage({ params }: Props) {
+  const { data } = await photosGet({ user: params.user });
+
+  if (!data) return null;
   return (
-    <main>
-      <h1>{params.user}</h1>
-    </main>
+    <section className="container mainContainer">
+      <h1 className="title">{params.user}</h1>
+      <Feed photos={data} user={params.user} />
+    </section>
   );
 }
